8d20f8e78fc5bed27b14a104e2a7c2476b4b6562
Andrew Welch <ajwpr5@gmail.com>
2016-02-12 10:01:44 +0000

    first

---
+++ prog.rb
@@ -0,0 +1,92 @@
+require 'rubygems'
+require 'sanitize'
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+
+
+#make array to hold the post contents for each post
+items = Array.new
+#make array for the noncommonwords
+noncommonwords = Array.new
+postwords = Array.new
+
+
+#get the file with nokogiri
+f = File.open("YOUR WORDPRESS EXPORT XML FILE")
+doc = Nokogiri::XML(f)
+
+
+#array holding common words
+commonwords = ['a', 'he', 'me', 'however', 'the','i', 'i', 'a','able','about','above','abroad','according','accordingly','across','actually','adj','after','afterwards','again','against','ago','ahead','ain\'t','all','allow','allows','almost','alone','along','alongside','already','also','although','always','am','amid','amidst','among','amongst','an','and','another','any','anybody','anyhow','anyone','anything','anyway','anyways','anywhere','apart','appear','appreciate','appropriate','are','aren\'t','around','as','a\'s','aside','ask','asking','associated','at','available','away','awfully','b','back','backward','backwards','be','became','because','become','becomes','becoming','been','before','beforehand','begin','behind','being','believe','below','beside','besides','best','better','between','beyond','both','brief','but','by','c','came','can','cannot','cant','can\'t','caption','cause','causes','certain','certainly','changes','clearly','c\'mon','co','co.','com','come','comes','concerning','consequently','consider','considering','contain','containing','contains','corresponding','could','couldn\'t','course','c\'s','currently','d','dare','daren\'t','definitely','described','despite','did','didn\'t','different','directly','do','does','doesn\'t','doing','done','don\'t','down','downwards','during','e','each','edu','eg','eight','eighty','either','else','elsewhere','end','ending','enough','entirely','especially','et','etc','even','ever','evermore','every','everybody','everyone','everything','everywhere','ex','exactly','example','except','f','fairly','far','farther','few','fewer','fifth','first','five','followed','following','follows','for','forever','former','formerly','forth','forward','found','four','from','further','furthermore','g','get','gets','getting','given','gives','go','goes','going','gone','got','gotten','greetings','h','had','hadn\'t','half','happens','hardly','has','hasn\'t','have','haven\'t','having','he','he\'d','he\'ll','hello','help','hence','her','here','hereafter','hereby','herein','here\'s','hereupon','hers','herself','he\'s','hi','him','himself','his','hither','hopefully','how','howbeit','however','hundred','i','i\'d','ie','if','ignored','i\'ll','i\'m','immediate','in','inasmuch','inc','inc.','indeed','indicate','indicated','indicates','inner','inside','insofar','instead','into','inward','is','isn\'t','it','it\'d','it\'ll','its','it\'s','itself','i\'ve','j','just','k','keep','keeps','kept','know','known','knows','l','last','lately','later','latter','latterly','least','less','lest','let','let\'s','like','liked','likely','likewise','little','look','looking','looks','low','lower','ltd','m','made','mainly','make','makes','many','may','maybe','mayn\'t','me','mean','meantime','meanwhile','merely','might','mightn\'t','mine','minus','miss','more','moreover','most','mostly','mr','mrs','much','must','mustn\'t','my','myself','n','name','namely','nd','near','nearly','necessary','need','needn\'t','needs','neither','never','neverf','neverless','nevertheless','new','next','nine','ninety','no','nobody','non','none','nonetheless','noone','no-one','nor','normally','not','nothing','notwithstanding','novel','now','nowhere','o','obviously','of','off','often','oh','ok','okay','old','on','once','one','ones','one\'s','only','onto','opposite','or','other','others','otherwise','ought','oughtn\'t','our','ours','ourselves','out','outside','over','overall','own','p','particular','particularly','past','per','perhaps','placed','please','plus','possible','presumably','probably','provided','provides','q','que','quite','qv','r','rather','rd','re','really','reasonably','recent','recently','regarding','regardless','regards','relatively','respectively','right','round','s','said','same','saw','say','saying','says','second','secondly','see','seeing','seem','seemed','seeming','seems','seen','self','selves','sensible','sent','serious','seriously','seven','several','shall','shan\'t','she','she\'d','she\'ll','she\'s','should','shouldn\'t','since','six','so','some','somebody','someday','somehow','someone','something','sometime','sometimes','somewhat','somewhere','soon','sorry','specified','specify','specifying','still','sub','such','sup','sure','t','take','taken','taking','tell','tends','th','than','thank','thanks','thanx','that','that\'ll','thats','that\'s','that\'ve','the','their','theirs','them','themselves','then','thence','there','thereafter','thereby','there\'d','therefore','therein','there\'ll','there\'re','theres','there\'s','thereupon','there\'ve','these','they','they\'d','they\'ll','they\'re','they\'ve','thing','things','think','third','thirty','this','thorough','thoroughly','those','though','three','through','throughout','thru','thus','till','to','together','too','took','toward','towards','tried','tries','truly','try','trying','t\'s','twice','two','u','un','under','underneath','undoing','unfortunately','unless','unlike','unlikely','until','unto','up','upon','upwards','us','use','used','useful','uses','using','usually','v','value','various','versus','very','via','viz','vs','w','want','wants','was','wasn\'t','way','we','we\'d','welcome','well','we\'ll','went','were','we\'re','weren\'t','we\'ve','what','whatever','what\'ll','what\'s','what\'ve','when','whence','whenever','where','whereafter','whereas','whereby','wherein','where\'s','whereupon','wherever','whether','which','whichever','while','whilst','whither','who','who\'d','whoever','whole','who\'ll','whom','whomever','who\'s','whose','why','will','willing','wish','with','within','without','wonder','won\'t','would','wouldn\'t','x','y','yes','yet','you','you\'d','you\'ll','your','you\'re','yours','yourself','yourselves','you\'ve','z','zero']
+
+
+
+#do the look up for the texts
+
+doc.css('item').each do |item|
+
+	content_text = item.at_xpath("content:encoded").text
+
+	#and for the link where the text appears
+
+	link = item.at_xpath("link").text
+
+	#for each item we split it into an array
+
+	wordsplit = content_text.split(" ")
+
+	#then for each item in that array sanitize the words
+
+	wordsplit.each do |word|
+
+		postwords.push([Sanitize.clean(word).gsub(/\s+/, "").gsub(/\W+/, '').downcase, link])
+
+	end
+
+	#noncommonwords is an array for sanitized words for this specific post
+
+end
+
+
+
+#delete items from noncommonwords if the key :word is in commonwords
+postwords.delete_if{|a, b| commonwords.include?(a)}
+
+
+#delete items from postwords if :word is empty
+postwords.delete_if{|a, b| a.empty?}
+
+
+#word count
+wordcount = Hash.new(0)
+
+
+postwords.each do |a, b|
+	wordcount[a] += 1
+end
+
+#sorted list of keyword count
+sortedfreqlist = Array.new
+
+wordcount.sort_by {|a,b|b}.each do |a,b|
+	sortedfreqlist.push([a, b])
+end
+
+#keyword and link
+keywordlinkbundle = Hash.new
+
+sortedfreqlist.each do |word, number|
+
+	wordlinkarray = Array.new
+
+	postwords.each do |lword,link|
+		if lword == word
+			wordlinkarray.push(link)
+		end
+	end
+
+	keywordlinkbundle[:key => [word, number]] = [:links => wordlinkarray]
+
+end
+#pp keywordlinkbundle
